2e9dc3bfd83e76577b4e18341b8c2dac8676684e
leandrogiova <leandrogiovacchini@gmail.com>
2022-08-08 17:54:16 -0300

    Funcion agregarProducto implementada

--- src/app/productos-principal/productos-principal.component.ts
+++ src/app/productos-principal/productos-principal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { Producto } from '../models/producto';
 
 @Component({
   selector: 'app-productos-principal',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosPrincipalComponent implements OnInit {
 
-  constructor() { }
+  agregarProducto: FormGroup;
+
+  constructor(private fb: FormBuilder, private firebase: AngularFirestore) {
+    this.agregarProducto = this.fb.group({
+      numeroProducto: '',
+      nombre: '',
+      precio: '',
+    });
+
+   }
 
   ngOnInit(): void {
+
   }
+
+
+
+  enviarProducto(){
+    console.log("formulario producto:", this.agregarProducto);
+    let producto1: Producto;
+    producto1 = this.agregarProducto.value;
+
+
+
+    this.firebase.collection('productos').add(producto1).then( () => {
+    }).catch(err => {
+      console.log("Ha ocurrido un problema\n", err);
+    })
+
+  }
+
 
 }
